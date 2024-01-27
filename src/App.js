e75331802f9cdcc51184d89e5cd50d329564b6f7
Rtwqey <brkyeoad@gmail.com>
2024-01-27 13:56:35 +0545

    git adder

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  function interaction(){
+    var i = 0;
+    alert('enter password', 
+    <input type='text' placeholder='Enter password'
+>
+    </input>)
+
+
+  }
+  return (
+    <div style={{ height:"15px", width:'45px', backgroundColor:'red', marginLeft:'30px'}} onClick={interaction} className='rocknroll'>
+      
+
+  
+
+          </div>
+  );
+}
+
+export default App;
